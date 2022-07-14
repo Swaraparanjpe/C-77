63cf3e648754f662a551720a5f5a5d86e1ae917e
Swara Paranjpe <99730038+Swaraparanjpe@users.noreply.github.com>
2022-07-14 17:16:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var first_name="smanthagunpowder";
+var last_name="masala powder";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var a=254798347589;
+var b =7487487748469797492479374816829478479371;
+var add=a+b;
+console.log(add);
+
